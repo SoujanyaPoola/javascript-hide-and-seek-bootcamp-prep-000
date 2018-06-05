2029f407b94547e495cdbeb09f5d6cfae7d270c8
Souji 2613 <puppypoola@gmail.com>
2018-06-05 16:27:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function getFirstSelector(selector) {
+/*function getFirstSelector(selector) {
   return document.querySelector(selector);
 }
 
@@ -10,7 +10,7 @@ function nestedTarget(){
    for (let i = 0; i < lis.length; i++) {
   lis[i].innerHTML = parseInt(lis[i].innerHTML)+(n) ;
  }
- }
+ }*/
  function deepestChild(){
    const div = document.getElementById("grand-node").querySelectorAll("div") ;
    var test ;
@@ -20,3 +20,31 @@ function nestedTarget(){
    return test;
    
  }
+ 
+ function getFirstSelector(selector){
+   return document.querySelector(selector);
+ }
+ 
+ function nestedTarget(){
+   return document.getElementById("nested").querySelector(".target");
+ }
+ 
+function increaseRankBy(n) {
+  const rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
+
+
+ 
+ 
+ 
+ 
+ 
+ 
